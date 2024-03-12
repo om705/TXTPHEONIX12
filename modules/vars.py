c6349a9f22903e6185b30608fe41cb67e89085bd
rahulkhatri00 <116899776+rahulkhatri00@users.noreply.github.com>
2024-03-12 14:39:17 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 6251412907))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6389388334").split()))
+API_ID = "27161113"
+API_HASH = "65c4b51f54049ebe22bc01fd9a95575b"
+BOT_TOKEN = "6806206800:AAHpkIk7pL3RyvJ4EOPS5Vbk2m79u03S6Ss"
+OWNER_ID = "6401497985"
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6401497985", "1366906399").split()))

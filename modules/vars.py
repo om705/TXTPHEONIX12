8028e13dfac0ce23822f7de2e4b9de15544d676f
om705 <61368509+om705@users.noreply.github.com>
2024-03-24 22:20:13 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "27161113"
+API_ID = "gdjdhdbdndb"
 API_HASH = "65c4b51f54049ebe22bc01fd9a95575b"
 BOT_TOKEN = "6806206800:AAHpkIk7pL3RyvJ4EOPS5Vbk2m79u03S6Ss"
 OWNER_ID = "6401497985"
